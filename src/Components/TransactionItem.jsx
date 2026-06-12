693560d6e7c77d09b24dd8ea17fe2f57774729e9
Arif <arifmarahman19@gmail.com>
2026-06-12 20:41:11 +0100

    TransactionItem component added

--- src/Components/TransactionItem.jsx
+++ src/Components/TransactionItem.jsx
@@ -1,18 +1,18 @@
 function TransactionItem({
-    item,
+    description,
     category,
     date,
-    price,
+    amount,
     ...props
 }){
     return(
-        <div className="w-full flex border rounded-md border-gray-300 px-5 py-1 justify-between hover:bg-gray-100" {...props}>
+        <div className="w-full flex border-b rounded-md border-gray-300 px-5 py-1 justify-between hover:bg-gray-100" {...props}>
             <div>
-                <h1 className="text-lg font-semibold text-black">{item}</h1>
+                <h1 className="text-lg font-semibold text-black">{description}</h1>
                 <p className="text-gray-500">{category}  • {date}</p>
             </div>
             <div className="text-red-500 text-lg py-2">
-                -£{price}
+                -£{amount}
             </div>
 
         </div>
