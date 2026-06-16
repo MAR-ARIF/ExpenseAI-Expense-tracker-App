29e8617a1f0e3f0d4c3fb33721f5350f9f5cd909
Arif <arifmarahman19@gmail.com>
2026-06-16 19:53:17 +0100

    Styles updated in Input Component

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -12,13 +12,13 @@ const Input = forwardRef(function Input({
         <div className="w-full">
             {label && <label
             htmlFor={id}
-            className="inline-block mb-1 pl-1 font-semibold text-2xl">
+            className="block mb-1 pl-1 font-medium text-md text-gray-900">
                 {label}
                 </label>}
             <input
             id={id}
             ref={ref}
-            className={`w-full border-2 border-gray-400 rounded-lg px-4 py-3 focus:outline-none focus:ring-1 focus:ring-blue-300 focus:border-transparent shadow-sm transition-all duration-200 ${className}` }
+            className={`w-full border border-gray-300 bg-white rounded-lg text-gray-900 px-3 py-2 focus:outline-none focus:ring-1 focus:ring-blue-300 focus:border-blue-300 shadow-sm transition-all duration-200 ${className}` }
             {...props}
             placeholder={placeholder}
             type={type}
