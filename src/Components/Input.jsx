432b4bf1e783d1cd77e0e5a0e3475bb8f8a0086a
Arif <arifmarahman19@gmail.com>
2026-06-19 21:01:54 +0100

    bg color added

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -9,7 +9,7 @@ const Input = forwardRef(function Input({
 },ref){
     const id = useId();
     return(
-        <div className="w-full">
+        <div className="w-ful">
             {label && <label
             htmlFor={id}
             className="block mb-1 pl-1 font-medium text-md text-gray-900">
@@ -18,7 +18,7 @@ const Input = forwardRef(function Input({
             <input
             id={id}
             ref={ref}
-            className={`w-full border border-gray-300 bg-white rounded-lg text-gray-900 px-3 py-2 focus:outline-none focus:ring-1 focus:ring-blue-300 focus:border-blue-300 shadow-sm transition-all duration-200 ${className}` }
+            className={`w-full border border-gray-300 bg-indigo-50 rounded-lg text-gray-900 px-3 py-2 focus:outline-none focus:ring-1 focus:ring-blue-300 focus:border-blue-300 shadow-sm transition-all duration-200 ${className}` }
             {...props}
             placeholder={placeholder}
             type={type}
