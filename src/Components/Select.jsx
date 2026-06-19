1602d8b4bf23d5536da0e77bae491d8a24aa1db8
Arif <arifmarahman19@gmail.com>
2026-06-19 21:02:07 +0100

    bg color added

--- src/Components/Select.jsx
+++ src/Components/Select.jsx
@@ -16,7 +16,7 @@ const Select = forwardRef(function Select({
             className="block mb-1 pl-1 font-medium text-md text-gray-900">
                 {label}
                 </label>}
-            <select id={id} ref={ref} {...props} className={`w-full border border-gray-300 bg-white rounded-lg text-gray-900 px-3 py-2 focus:outline-none focus:ring-1 focus:ring-blue-300 focus:border-blue-300 placeholder:text-gray-900 shadow-sm transition-all duration-200 ${className}` }>
+            <select id={id} ref={ref} {...props} className={`w-full border border-gray-300 bg-indigo-50 rounded-lg text-gray-900 px-3 py-2 focus:outline-none focus:ring-1 focus:ring-blue-300 focus:border-blue-300 placeholder:text-gray-900 shadow-sm transition-all duration-200 ${className}` }>
                {placeholder && 
                 <option className="text-gray-400" value="" disabled>
                     {placeholder}
