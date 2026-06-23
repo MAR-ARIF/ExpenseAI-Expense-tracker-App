c9d12b5edae4dd32e4bd06d664aefda1607bcd7e
Arif <arifmarahman19@gmail.com>
2026-06-23 16:44:59 +0100

    Transaction Item modified for dashboard page

--- src/Components/TransactionItem.jsx
+++ src/Components/TransactionItem.jsx
@@ -6,7 +6,7 @@ function TransactionItem({
     ...props
 }){
     return(
-        <div className="w-full flex border-b rounded-md border-gray-300 px-5 py-1 justify-between hover:bg-gray-100" {...props}>
+        <div className="w-full flex border-b rounded-md border-gray-300 px-5 py-1 justify-between hover:bg-gray-100">
             <div>
                 <h1 className="text-lg font-semibold text-black">{description}</h1>
                 <p className="text-gray-500">{category}  • {date}</p>
