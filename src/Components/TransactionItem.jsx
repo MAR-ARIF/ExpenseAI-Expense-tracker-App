58bf04a0c44cd945de3e86577a427d71d5df0fbc
Arif <arifmarahman19@gmail.com>
2026-06-24 22:24:03 +0100

    Data formate fixed

--- src/Components/TransactionItem.jsx
+++ src/Components/TransactionItem.jsx
@@ -9,7 +9,11 @@ function TransactionItem({
         <div className="w-full flex border-b rounded-md border-gray-300 px-5 py-1 justify-between hover:bg-gray-100">
             <div>
                 <h1 className="text-lg font-semibold text-black">{description}</h1>
-                <p className="text-gray-500">{category}  • {date}</p>
+                <p className="text-gray-500">{category}  • {new Date(date).toLocaleDateString("en-GB" , {
+                    day: "numeric",
+                    month : "long",
+                    year : "numeric"
+                })}</p>
             </div>
             <div className="text-red-500 text-lg py-2">
                 -£{amount}
