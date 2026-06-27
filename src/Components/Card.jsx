476ff6cb8710d92e94095c75a068f0f4e8c80f5b
Arif <arifmarahman19@gmail.com>
2026-06-27 21:33:03 +0100

    Card modified with styles added as budgetCard need

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -6,9 +6,9 @@ function Card({
     ...props
 }){
     return (
-        <div className={`bg-white w-80 p-5  border border-gray-200 rounded-xl`}>
+        <div className="bg-white w-80 p-5  border border-gray-200 rounded-xl">
             <p className="text-gray-600">{header}</p>
-            <p className="text-2xl font-bold pt-1 pb-1">{body}</p>
+            <p className={`text-2xl font-bold pt-1 pb-1 ${className}`}>{body}</p>
             <p className="text-gray-600">{footer}</p>
     
         </div>
