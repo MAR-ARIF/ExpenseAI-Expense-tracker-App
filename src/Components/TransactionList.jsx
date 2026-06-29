554332f2bacd12d04b6eab93e29f9eeb4473301a
Arif <arifmarahman19@gmail.com>
2026-06-29 11:11:14 +0100

    style modified

--- src/Components/TransactionList.jsx
+++ src/Components/TransactionList.jsx
@@ -16,7 +16,7 @@ function TransactionList(){
         })
     },[]);
     return(
-        <div className=" mx-auto bg-white border border-gray-200 rounded-xl mt-10">
+        <div className=" mx-auto bg-white border border-gray-200 rounded-xl mt-5">
             <h1 className="text-2xl m-4 font-semibold">Recent Transactions</h1>
             {expenses.map((expense) => (
                 <div key={expense.$id}>
