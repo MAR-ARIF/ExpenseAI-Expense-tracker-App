92f232aa027ab044168499fb6d6ed12c0fc41715
Arif <arifmarahman19@gmail.com>
2026-07-03 21:31:32 +0100

    Transaction Page style modified for mobile

--- src/Pages/TransactionPage.jsx
+++ src/Pages/TransactionPage.jsx
@@ -11,26 +11,28 @@ function TransactionPage(){
     const categories = ["All", "Foods" , "Grocery" , "Transportation", "Shopping" , "Utilities" , "Entertainment" , "Health"]
     
     return(
-        <Container>
-            <h1 className="text-2xl font-semibold mb-4">Transactions</h1>
-            <div className="flex gap-3 ">
-                {categories.map((c) => (
-                    <div key={c}>
-                       <button 
-                       onClick={() => setSelected(c)}
-                       className={`border border-gray-200 px-4 py-0.5 rounded-xl text-md font-semibold ${selected == c ? "bg-indigo-500 text-white" : "bg-slate-50 hover:bg-slate-100"} `}
-                       >
-                            {c}
-                       </button>
-                    </div>
-                ))}
+        <div className="py-8">
+            <Container>
+                <h1 className="text-2xl font-semibold mb-4">Transactions</h1>
+                <div className="flex gap-3 flex-wrap ">
+                    {categories.map((c) => (
+                        <div key={c}>
+                        <button 
+                        onClick={() => setSelected(c)}
+                        className={`border border-gray-200 px-4 py-0.5 rounded-xl text-md font-semibold ${selected == c ? "bg-indigo-500 text-white" : "bg-slate-50 hover:bg-slate-100"} `}
+                        >
+                                {c}
+                        </button>
+                        </div>
+                    ))}
 
 
-            </div>
-            <TransactionList hideHeader={true} category={selected} />
-            
-            
-        </Container>
+                </div>
+                <TransactionList hideHeader={true} category={selected} />
+                
+                
+            </Container>
+        </div>
     )
 }
 export default TransactionPage
