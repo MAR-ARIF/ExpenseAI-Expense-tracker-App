d4c887b380f77920829effe8ac41fd4dcde831dd
Arif <arifmarahman19@gmail.com>
2026-07-08 20:04:10 +0100

    SignUpPage added

---
+++ src/Pages/SignUpPage.jsx
@@ -0,0 +1,16 @@
+import SignUpForm from "../Components/SignUpForm";
+import Container from "../Container/Container";
+
+
+function SignUpPage(){
+    return(
+        <div className="min-h-screen flex items-center justify-center">
+            <Container>
+                <SignUpForm />
+            </Container>
+        
+        
+        </div>
+    )
+}
+export default SignUpPage
