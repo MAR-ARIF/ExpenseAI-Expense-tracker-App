dd135ce429998ccf125e85f4423da4c73cf4ce95
Arif <arifmarahman19@gmail.com>
2026-07-09 21:38:54 +0100

    App.jsx added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
+
+import { Outlet } from "react-router-dom"
 import Header from "./Header/Header"
+import { useEffect, useState } from "react"
+import authService from "./appwrite/auth"
+import { useDispatch } from "react-redux"
+import { login, logout } from "./slices/authSlice"
+
+
 
 
 function App() {
+  const [loading , setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService.getUser().then((userData) => {
+      if (userData){
+        dispatch(login(userData))
+      } else {
+        dispatch(logout());
+      }
+    })
+    .finally(() => {setLoading(false)})
+  } , [])
  
 
-  return (
-    <>
+  return !loading ?  (
+    <div className="bg-slate-100 min-h-screen">
+    
       <Header />
-    </>
-  )
+      <Outlet />
+
+    </div>
+  ) : null
 }
 
 export default App
