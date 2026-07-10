a15567f2bc3382190642cea13873ec95db3bf33f
Arif <arifmarahman19@gmail.com>
2026-07-10 11:31:09 +0100

    Final modification added

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,62 @@ import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import DashboardPage from './Pages/DashboardPage.jsx'
+import AuthLayout from './Components/AuthLayout.jsx'
+import TransactionPage from './Pages/TransactionPage.jsx'
+import AnalyticsPage from './Pages/AnalyticsPage.jsx'
+import InsightPage from './Pages/InsightPage.jsx'
+import LoginPage from './Pages/LoginPage.jsx'
+import SignUpPage from './Pages/SignUpPage.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element : <App />,
     children : [
+      {
+        path : "/",
+        element : (
+          <AuthLayout authentication>
+            <DashboardPage />
+          </AuthLayout>
+        )
+      },
+      {path: "/transactions",
+        element : (
+          <AuthLayout authentication >
+            <TransactionPage />
+          </AuthLayout>
+        )
+      } ,
+      {path:"/analytics",
+        element :(
+          <AuthLayout authentication>
+            <AnalyticsPage />
+          </AuthLayout>
+        )
+      },
+      {path:"/ai-insights",
+        element: (
+          <AuthLayout authentication>
+            <InsightPage />
+          </AuthLayout>
+        )
+      },{
+        path: "/login",
+        element:(
+          <AuthLayout authentication={false}>
+            <LoginPage />
+          </AuthLayout>
+        )
+      },
+      {path :"/signup",
+        element:(
+          <AuthLayout authentication={false}>
+            <SignUpPage />
+          </AuthLayout>
+        )
+      }
 
     ]
   }
