40a53d3ae7f92b5007230826bf4d9745573770fd
Arif <arifmarahman19@gmail.com>
2026-07-12 21:36:35 +0100

    Added default style for charts

--- src/Components/ExpensePieChart.jsx
+++ src/Components/ExpensePieChart.jsx
@@ -1,3 +1,4 @@
+import { PieChartIcon } from "lucide-react";
 import {
     PieChart,
     Pie,
@@ -17,10 +18,28 @@ const COLORS = [
     "#84CC16", 
 ];
 
-function ExpensePieChart({ data }) {
+function ExpensePieChart({ data , isNoTransaction = false }) {
     const maxAmount = Math.max(...data.map((d) => d.amount));
 
-    return (
+    return isNoTransaction ? 
+    <div className="bg-white p-4 border border-gray-200 rounded-xl">
+      <div className="h-72 flex flex-col items-center justify-center">
+        <PieChartIcon className="w-16 h-16 text-gray-400 mb-4" />
+
+        <p className="font-semibold">
+            No spending data
+        </p>
+
+        <p className="text-gray-500 text-sm">
+            Your charts will appear here.
+        </p>
+      </div>
+
+    </div>
+    
+
+    :
+    (
         <div className="bg-white border border-gray-200 rounded-xl p-5">
             <h2 className="text-xl font-semibold mb-4">
                 Spending by Category
