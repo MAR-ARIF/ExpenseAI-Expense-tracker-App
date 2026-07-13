98b86e8a433997eba7466d9a32c0607b775d670f
Arif <arifmarahman19@gmail.com>
2026-07-13 21:35:12 +0100

    Header completed with mobile styles

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -12,20 +12,19 @@ function Header(){
         {name: "Dashboard" , path: "/" , active : authStatus , icon: <LayoutDashboard className={className}/> },
         {name : "Transactions" , path: "/transactions" , active : authStatus , icon : <List className={className}/> },
         {name : "Analytics" , path: "/analytics" , active : authStatus , icon : <ChartColumn className={className}/>},
-        {name : "Insights" , path: "/ai-insights" , active : authStatus , icon : <Sparkles className={className}/>},
-        //{name : "SignUp" , path: "/signup" , active : !authStatus , icon : <UserPlus className={className}/>},
-        //{name: "Login" , path: "/login" , active : !authStatus , icon: <LogIn className={className}/>}
+        {name : "Insights" , path: "/ai-insights" , active : authStatus , icon : <Sparkles className={className}/>}
+       
     ]
     return (
-        <header className="w-full bg-white/95 border-b border-gray-200 py-1.5"> 
+        <div className="w-full fixed top-0 z-50 bg-white/95 border-b border-gray-200 py-1.5"> 
             <Container>
-                <div className="grid grid-cols-3 items-center">
+                <div className="grid grid-cols-2 md:grid-cols-3 items-center">
                     
                     <Link to="/"className="flex font-semibold text-xl text-indigo-600 gap-1">
                         <Sparkles /> ExpenseAI
                     </Link>
                     
-                    <ul className="flex justify-center items-center text-gray-500 font-semibold gap-2">
+                    <ul className="justify-center hidden md:flex items-center text-gray-500 font-semibold gap-2">
                         {navItems.map((item) => 
                             item.active ? (
                                 <li key={item.name}>
@@ -51,12 +50,37 @@ function Header(){
                         )
                     }
                 </div>
+                
+           
                         
             </Container>
-           
+            <div className="md:hidden w-full z-50 border-t border-gray-200  fixed bottom-0 bg-white/95 ">
+                <Container>    
+                    <ul className="flex items-center justify-center mt-3 mb-3 text-gray-500 font-semibold gap-4">
+                        {navItems.map((item) => 
+                            item.active ? (
+                                <li key={item.name}>
+                                    <NavLink 
+                                    to={item.path}
+                                    className={({isActive}) => 
+                                        `flex flex-col items-center px-3 py-1 text-sm rounded-xl ${ isActive ? 
+                                            `bg-indigo-100 text-indigo-500` : `hover:bg-gray-100`
+                                        }` 
+                                    }
+                                    >
+                                        {item.icon} 
+                                        {item.name}
+                                    </NavLink>
+                                </li>
+                            ) : null
+                        )}
+                    </ul>
+                </Container>
+
+            </div>        
            
         
-        </header>
+        </div>
     )
 }
 export default Header
